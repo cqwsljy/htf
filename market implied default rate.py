cf2caf628132dd8f0cd1f8b1161c0cc0b754117c
Jiayong <cqwsljy@163.com>
2017-10-26 08:46:47 +0800

    '20171026'

--- market implied default rate.py
+++ market implied default rate.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-
+'''
 def getCashflow(upb,rate,cdr,sev):
     smm = 1 - np.power(1-cdr,1/Freq)
     inte = upb * rate/Freq
@@ -13,6 +13,41 @@ def getCashflow(upb,rate,cdr,sev):
 
 def getDiscount(timeV,rfr,freq):
     return 1/np.power(1+rfr/freq,timeV)
+'''
+def getCashflow(upb,rate,cdr,sev,rfr,freq,PeriodMax=19):
+    # initial
+    df = pd.DataFrame(np.zeros((PeriodMax,len(columns))),columns=columns)
+    df['Period'] = range(0,PeriodMax)
+    df.loc[0,'Balance'] = upb
+    df['Discount'] = getDiscount(range(0,PeriodMax),rfr,freq)
+    df.loc[0,'AssetDiscounting'] = 1 
+
+    SMM = 1 - np.power(1-cdr,1/freq)
+    df.loc[index,'Interest'] = df.loc[index-1,'Balance'] * Coupon / freq
+    df.loc[index,'Default'] = df.loc[index-1,'Balance'] * SMM
+    df.loc[index,'Loss'] = df.loc[index,'Default'] * sev
+    df.loc[index,'Principal'] = df.loc[index,'Default'] - df.loc[index,'Loss']
+    df.loc[index,'CashFlow'] = df.loc[index,'Interest'] + df.loc[index,'Principal']
+    df.loc[index,'Discount'] = 1/np.power(1+rfr/freq,df.loc[index,'Period'])
+    df.loc[index,'AssetDiscounting'] = 1/np.power(1+AssetFunding,index)
+    df.loc[index,'Balance'] = df.loc[index-1,'Balance'] - df.loc[index,'Principal'] - df.loc[index,'Loss']
+    if index == PeriodStop:
+        df.loc[index,'Principal'] = df.loc[index-1,'Balance'] - df.loc[index,'Loss']
+        df.loc[index,'CashFlow'] = df.loc[index,'Interest'] + df.loc[index,'Principal']
+        df.loc[index,'Discount'] = 1/np.power(1+rfr/freq,df.loc[index,'Period'])
+        df.loc[index,'AssetDiscounting'] = 1/np.power(1+AssetFunding,index)
+        df.loc[index,'Balance'] = df.loc[index-1,'Balance'] - df.loc[index,'Principal'] - df.loc[index,'Loss']
+        break
+    Price = sum(df.loc[:,'CashFlow']*df.loc[:,'Discount'])/df.loc[0,'Balance']*100
+    CumDefault = sum(df.loc[:,'Default'])/df.loc[0,'Balance']
+    df.loc[index,"AssetCashFlow"] = df.loc[0,'Balance']*((1 - CumDefault) +AssetSev*(1-AssetSev)*CumDefault)
+    CumLoss = sum(df.loc[:,'Loss'])/df.loc[0,'Balance']
+    CheckCumSev = CumLoss/CumDefault
+    AssetPrice = sum(df.loc[:,'AssetCashFlow']*df.loc[:,'AssetDiscounting'])/df.loc[0,'Balance']*100
+    return df
+
+def getDiscount(timeV,rfr,freq):
+    return 1/np.power(1+rfr/freq,timeV) #timeV could be a vector or a scalar
 
 # Given
 PeriodMax = 19
@@ -37,12 +72,7 @@ columns = ['Period','Balance','Interest','Principal',
 'Default','Loss','CashFlow','Discount','AssetCashFlow',
 'AssetDiscounting']
 
-# initial
-df = pd.DataFrame(np.zeros((PeriodMax,len(columns))),columns=columns)
-df['Period'] = range(0,PeriodMax)
-df.loc[0,'Balance'] = 1000000
-df['Discount'] = getDiscount(range(0,PeriodMax),RFR,Freq)
-df.loc[0,'AssetDiscounting'] = 1 
+
 '''
 for index in df.index[1:]:
     df.loc[index,'Interest'] = df.loc[index-1,'Balance'] * Coupon / Freq
@@ -60,7 +90,7 @@ for index in df.index[1:]:
         df.loc[index,'AssetDiscounting'] = 1/np.power(1+AssetFunding,index)
         df.loc[index,'Balance'] = df.loc[index-1,'Balance'] - df.loc[index,'Principal'] - df.loc[index,'Loss']
         break
-'''
+
 for index in df.index[1:]:
     upb = df.loc[index-1,'Balance']
     [bal,inte,defa,prin,cashflow,loss] = getCashflow(upb,Coupon,CDR,SEV) #getCashflow(upb,rate,cdr,sev)
@@ -77,10 +107,13 @@ for index in df.index[1:]:
         df.loc[index,'Discount'] = 1/np.power(1+RFR/Freq,df.loc[index,'Period'])
         df.loc[index,'AssetDiscounting'] = 1/np.power(1+AssetFunding,index)
         df.loc[index,'Balance'] = df.loc[index-1,'Balance'] - df.loc[index,'Principal'] - df.loc[index,'Loss']
-        break
+        break        
 Price = sum(df.loc[:,'CashFlow']*df.loc[:,'Discount'])/df.loc[0,'Balance']*100
 CumDefault = sum(df.loc[:,'Default'])/df.loc[0,'Balance']
 df.loc[index,"AssetCashFlow"] = df.loc[0,'Balance']*((1 - CumDefault) +AssetSev*(1-AssetSev)*CumDefault)
 CumLoss = sum(df.loc[:,'Loss'])/df.loc[0,'Balance']
 CheckCumSev = CumLoss/CumDefault
 AssetPrice = sum(df.loc[:,'AssetCashFlow']*df.loc[:,'AssetDiscounting'])/df.loc[0,'Balance']*100
+
+'''
+
